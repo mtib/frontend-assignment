d9df471b7b126a018056d854d957315d1d8a02b4
Markus Becker <mtib.becker@gmail.com>
2021-09-07 17:47:00 +0200

    Use endpoint for user data

--- solution/src/Challenge.js
+++ solution/src/Challenge.js
@@ -4,15 +4,20 @@ import { hot } from 'react-hot-loader/root';
 import UserList from "./UserList";
 
 class Challenge extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userEndpoint: "https://jsonplaceholder.typicode.com/users",
+    };
+  }
   render() {
-    const { name } = this.props;
     return (
       <div className="responsiveContainer">
         <h1>
           Users
         </h1>
         <div>
-          <UserList />
+          <UserList userEndpoint={this.state.userEndpoint} />
         </div>
       </div>
     );
--- solution/src/UserList.js
+++ solution/src/UserList.js
@@ -4,14 +4,62 @@ import User from "./User";
 class UserList extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            users: null,
+            error: null,
+        };
     }
+
+    componentDidMount() {
+        // Fetch user data and clean up already
+        fetch(this.props.userEndpoint).then(data => {
+            return data.json();
+        }).then(json => {
+            this.setState({
+                users: json.map(user => {
+                    let simple_user = {
+                        name: user.name,
+                        website: user.website,
+                        thumbnail: "https://via.placeholder.com/150/1F83B3",
+                        company: null,
+                    }
+                    if ("company" in user) {
+                        simple_user.company = user.company.name;
+                    }
+                    return simple_user;
+                }),
+            });
+        }).catch(reason => {
+            this.setState({
+                error: `Fetching users failed: ${reason}`,
+            });
+        });
+    }
+
     render() {
-        return (
-            <div className="userlist">
-                <User thumbnail="https://via.placeholder.com/150/1F83B3" name="Markus Becker" website="https://blog.mtib.dev/" company="Applicant" />
+        if (this.state.error != null) {
+            return <div class="alert alert-danger" role="alert">
+                {this.state.error}
             </div>
-        );
+        }
+        else if (this.state.users == null) {
+            return <span>Loading...</span>
+        } else {
+            const users = this.state.users.map(user => {
+                return <User
+                    thumbnail={user.thumbnail}
+                    name={user.name}
+                    website={user.website}
+                    company={user.company}
+                />
+            })
+            console.log(this.state.users);
+            return (
+                <div className="userlist">
+                    {users}
+                </div>
+            );
+        }
     }
 }
 
