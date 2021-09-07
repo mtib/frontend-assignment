22265a797c61619ccf5f71ed2b61266f3d95a9ea
Markus Becker <mtib.becker@gmail.com>
2021-09-07 17:33:12 +0200

    Single user visually correct

--- solution/src/User.js
+++ solution/src/User.js
@@ -8,21 +8,21 @@ class User extends React.Component {
     render() {
         let name = <span className="User-name">{this.props.name}</span>
         if (this.props.website != null) {
-            name = <a href={this.props.website}>{name}</a>
+            name = <a className="User-name" href={this.props.website}>{this.props.name}</a>
         }
 
         let company = <span></span>
         if (this.props.company != null) {
-            company = <span className="User-company">{this.props.company}</span>
+            company = <span className="User-company">({this.props.company})</span>
         }
 
         return (
-            <div className="User">
-                <div className="d-flex">
+            <div className="User d-block">
+                <div className="d-flex align-items-center d-block">
                     <img src={this.props.thumbnail} className="User-thumbnail flex-shrink-1" />
-                    <div className="w-100">
+                    <span className="w-100 text-truncate">
                         {name} {company}
-                    </div>
+                    </span>
                 </div>
             </div>
         );
